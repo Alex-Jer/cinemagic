53371327cce168bf12847fdfc27de1ac6060db47
Alexandre <alexandrejeronimo2001@gmail.com>
2022-06-03 15:44:19 +0100

    components

---
+++ app/View/Components/Dashboard/Input.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components\Dashboard;
+
+use Illuminate\View\Component;
+
+class Input extends Component
+{
+    public $label;
+    public $placeholder;
+    public $name;
+    public $attr;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($label, $placeholder = null, $name, $attr = null)
+    {
+        $this->label = $label;
+        $this->placeholder = $placeholder;
+        $this->name = $name;
+        $this->attr = $attr;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.dashboard.input');
+    }
+}
