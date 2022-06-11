9e3647087920a4ad0d1fefddc853813b0ffcedb7
Alexandre <alexandrejeronimo2001@gmail.com>
2022-06-11 01:24:41 +0100

    tirar carrinho da sidebar para admins

--- resources/views/components/dashboard/sidebar-content.blade.php
+++ resources/views/components/dashboard/sidebar-content.blade.php
@@ -6,13 +6,15 @@
     </svg>
 </x-dashboard.sidebar-item>
 
-<x-dashboard.sidebar-item label="Carrinho" route="cart.index">
-    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-        </path>
-    </svg>
-</x-dashboard.sidebar-item>
+@if (!Auth::user()->tipo === 'A')
+    <x-dashboard.sidebar-item label="Carrinho" route="cart.index">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
+            </path>
+        </svg>
+    </x-dashboard.sidebar-item>
+@endif
 
 @if (Auth::user()->tipo === 'C')
     <x-dashboard.sidebar-group label="Histórico">
@@ -76,8 +78,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z">
                 </path>
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z">
                 </path>
             </svg>
         </x-dashboard.sidebar-group-item>
@@ -91,8 +92,8 @@
     </x-dashboard.sidebar-item>
 
     <x-dashboard.sidebar-item label="Estatísticas" {{-- TODO: route="stats" --}}>
-        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
-            stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            viewBox="0 0 24 24" stroke="currentColor">
             <path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
             <path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
         </svg>
