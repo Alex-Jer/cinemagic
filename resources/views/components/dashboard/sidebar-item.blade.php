0d35f2df09881e3ec75413105a56619a20caaa3d
Alexandre <alexandrejeronimo2001@gmail.com>
2022-06-11 16:24:32 +0100

    sidebar fix

--- resources/views/components/dashboard/sidebar-item.blade.php
+++ resources/views/components/dashboard/sidebar-item.blade.php
@@ -3,7 +3,7 @@
         class="absolute {{ request()->routeIs($route) ? 'inset-y-0' : '' }} left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
         aria-hidden="true">
     </span>
-    <a class="{{ request()->is(strtok($route, '.')) ? 'text-gray-800 dark:text-gray-100' : '' }} inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+    <a class="{{ request()->routeIs($route) ? 'text-gray-800 dark:text-gray-100' : '' }} inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
         href="@if ($route) {{ route($route) }} @endif">
         {{ $slot }}
         <span class="ml-4"> {{ $label }} </span>
