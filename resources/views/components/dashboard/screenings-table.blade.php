216efc431b6f5ca1278e24986cafd5b6a473a2e7
Alexandre <alexandrejeronimo2001@gmail.com>
2022-06-14 00:26:10 +0100

    screenings table design fix

--- resources/views/components/dashboard/screenings-table.blade.php
+++ resources/views/components/dashboard/screenings-table.blade.php
@@ -1,4 +1,4 @@
-<table class="w-full whitespace-no-wrap table-fixed">
+<table class="w-full whitespace-no-wrap">
     <thead>
         <tr
             class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase bg-white border-b dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
@@ -6,9 +6,9 @@
             <th class="px-4 py-3">Dia</th>
             <th class="px-4 py-3">Hora</th>
             @if (!Auth::user())
-                <th class="w-2/12 px-4 py-3">Comprar bilhete</th>
+                <th class="xl:w-3/12 px-4 py-3">Comprar bilhete</th>
             @elseif (Auth::user()->isCustomer())
-                <th class="w-2/12 px-4 py-3">Comprar bilhete</th>
+                <th class="xl:w-3/12 px-4 py-3">Comprar bilhete</th>
             @endif
         </tr>
     </thead>
@@ -36,12 +36,10 @@
                     @if (!Auth::user())
                         <td class="px-4 py-3">
                             <div class="flex items-center space-x-4 text-sm">
-                                <form method="POST" action="{{ route('cart.store', $screening) }}"
-                                    class="mt-3">
+                                <form method="POST" action="{{ route('cart.store', $screening) }}" class="mt-3">
                                     @csrf
                                     <x-dashboard.button label="Comprar bilhete">
-                                        <svg class="w-5 h-5 mr-1.5" fill="none" stroke="currentColor"
-                                            viewBox="0 0 24 24">
+                                        <svg class="w-5 h-5 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z">
                                             </path>
@@ -57,8 +55,7 @@
                                 {{-- @csrf --}}
                                 <a href="{{ route('screenings.show', $screening) }}">
                                     <x-dashboard.button label="Comprar bilhete">
-                                        <svg class="w-5 h-5 mr-1.5" fill="none" stroke="currentColor"
-                                            viewBox="0 0 24 24">
+                                        <svg class="w-5 h-5 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z">
                                             </path>
