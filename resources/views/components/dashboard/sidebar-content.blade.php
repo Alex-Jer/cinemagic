9b29c9a70c817e7326887377fafe0f25eb67bae4
tgn <trwygon@outlook.pt>
2022-06-15 18:48:25 +0100

    sidebar employee perms

--- resources/views/components/dashboard/sidebar-content.blade.php
+++ resources/views/components/dashboard/sidebar-content.blade.php
@@ -6,7 +6,7 @@
     </svg>
 </x-dashboard.sidebar-item>
 
-@if (!Auth::user() || (Auth::user() && !Auth::user()->isAdmin()))
+@if (!Auth::user() || (Auth::user() && Auth::user()->isCustomer()))
     <x-dashboard.sidebar-item label="Carrinho" route="cart">
         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -47,6 +47,23 @@
     </x-dashboard.sidebar-item>
 @endif
 
+@if (Auth::user() && Auth::user()->isEmployee())
+    <x-dashboard.sidebar-item label="Sessões">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z">
+            </path>
+        </svg>
+    </x-dashboard.sidebar-item>
+    <x-dashboard.sidebar-item label="Utilizadores" route="admin.users">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z">
+            </path>
+        </svg>
+    </x-dashboard.sidebar-item>
+@endif
+
 @if (Auth::user() && Auth::user()->isAdmin())
     <x-dashboard.sidebar-group label="Gestão">
         <x-dashboard.sidebar-group-item label="Salas" route="admin.screen">
@@ -64,7 +81,6 @@
                 </path>
             </svg>
         </x-dashboard.sidebar-group-item>
-
         <x-dashboard.sidebar-group-item label="Sessões">
             <svg class="w-5 h-5 mt-px mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -72,13 +88,13 @@
                 </path>
             </svg>
         </x-dashboard.sidebar-group-item>
-
         <x-dashboard.sidebar-group-item label="Definições">
             <svg class="w-5 h-5 mt-px mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z">
                 </path>
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z">
                 </path>
             </svg>
         </x-dashboard.sidebar-group-item>
@@ -92,8 +108,8 @@
     </x-dashboard.sidebar-item>
 
     <x-dashboard.sidebar-item label="Estatísticas" {{-- TODO: route="stats" --}}>
-        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            viewBox="0 0 24 24" stroke="currentColor">
+        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
+            stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
             <path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
             <path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
         </svg>
