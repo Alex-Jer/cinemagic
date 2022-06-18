817c3315f970d3e227135968dc3a311a4bbe8173
Alexandre <alexandrejeronimo2001@gmail.com>
2022-06-18 16:07:49 +0100

    file input design fix

--- resources/views/components/dashboard/file-input.blade.php
+++ resources/views/components/dashboard/file-input.blade.php
@@ -1,4 +1,4 @@
 <div class="mt-1 mb-5 mr-2 w-80">
-    <input class="block w-full transition border border-solid input-primary bg-clip-padding" type="file"
+    <input class="block w-full px-3 transition border border-solid rounded input-primary bg-clip-padding py-1.5" type="file"
         name="{{ $name }}">
 </div>
