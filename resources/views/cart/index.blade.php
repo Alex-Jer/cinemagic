abd4b3ca0ecf6085fa2478bbc5980ef3bc57b678
Alexandre <alexandrejeronimo2001@gmail.com>
2022-06-19 19:22:34 +0100

    botão pagamento design fix

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -12,15 +12,28 @@
             <div class="float-right text-sm">
                 <form method="POST" action="/cart">
                     @csrf
-                    <x-dashboard.button class="{{ Auth::guest() ? 'button-disabled' : 'button-primary' }}" disabled>
-                        <svg class="w-5 h-5 mt-px mr-1" data-darkreader-inline-stroke="" fill="none" stroke="currentColor"
-                            style="--darkreader-inline-stroke: currentColor;" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"></path>
-                        </svg>
-                        <x-slot:label>Pagamento</x-slot:label>
-                        <x-slot:tooltip>Inicie sessão para proceder ao pagamento</x-slot:tooltip>
-                    </x-dashboard.button>
+                    @if (Auth::guest())
+                        <x-dashboard.button class="button-disabled" disabled>
+                            <svg class="w-5 h-5 mt-px mr-1" data-darkreader-inline-stroke="" fill="none" stroke="currentColor"
+                                style="--darkreader-inline-stroke: currentColor;" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z">
+                                </path>
+                            </svg>
+                            <x-slot:label>Pagamento</x-slot:label>
+                            <x-slot:tooltip>Inicie sessão para proceder ao pagamento</x-slot:tooltip>
+                        </x-dashboard.button>
+                    @else
+                        <x-dashboard.button class="button-primary">
+                            <svg class="w-5 h-5 mt-px mr-1" data-darkreader-inline-stroke="" fill="none" stroke="currentColor"
+                                style="--darkreader-inline-stroke: currentColor;" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z">
+                                </path>
+                            </svg>
+                            <x-slot:label>Pagamento</x-slot:label>
+                        </x-dashboard.button>
+                    @endif
                 </form>
             </div>
         </div>
