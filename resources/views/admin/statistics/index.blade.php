711a9eafa4ad8de871f52185658d39ee9613f553
Alexandre <alexandrejeronimo2001@gmail.com>
2022-06-19 23:30:49 +0100

    estatísticas (teste)

--- resources/views/admin/statistics/index.blade.php
+++ resources/views/admin/statistics/index.blade.php
@@ -1,3 +1,27 @@
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+    <script>
+        var test = 100;
+    </script>
+    <script src="{{ asset('js/charts-lines.js') }}" defer></script>
+</head>
 <x-dashboard.layout title="CineMagic - Estatísticas" header="Estatísticas">
-
+    <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+        <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
+            Lines
+        </h4>
+        <canvas id="line"></canvas>
+        <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
+            <!-- Chart legend -->
+            <div class="flex items-center">
+                <span class="inline-block w-3 h-3 mr-1 bg-teal-500 rounded-full"></span>
+                <span>Organic</span>
+            </div>
+            <div class="flex items-center">
+                <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
+                <span>Paid</span>
+            </div>
+        </div>
+    </div>
 </x-dashboard.layout>
